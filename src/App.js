e6892dbc2adc045efe3613bb177469dd85c1303f
Julio Martins de Sousa <jmasousa@voegol.com.br>
2020-02-02 18:57:51 -0300

    Create basic style of template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<p>Basic styles template</p>
+			<p>Content:</p>
+			<ul>
+				<li>.editorconfig</li>
+				<li>.eslintrc.js</li>
+				<li>.prettierrc</li>
+			</ul>
+
+			<code>
+				// ES Lint installation <br />
+				>> yarn add eslint -D <br />
+				>> yarn eslint --init <br />
+				<br />
+				// Prettier installation <br />
+				>> yarn add prettier eslint-config-prettier eslint-plugin-prettier
+				babel-eslint -D <br />
+				<br />
+				// VS Code Settings: <br />
+				"editor.codeActionsOnSave":{' '}
+				{`{
+		"source.fixAll": true
+	}`}
+				<br />
+				<br />
+				Copy/paste or compare the files listed above and have fun!
+			</code>
+		</div>
+	);
 }
 
 export default App;
